6b7e4e978aa5a18effc3be6cc788dc2187c7b949
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-04-29 17:38:14 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+import cornflake
+
+setup(
+    name='cornflake',
+    version=cornflake.__version__,
+    long_description=cornflake.__doc__,
+    author='Rupert Bedford',
+    author_email='rupert.bedford@renalregistry.nhs.uk',
+    url='https://www.renalreg.org/',
+    packages=find_packages(),
+    install_requires=[
+        'bleach',
+        'Delorean',
+        'pytz',
+        'six',
+    ],
+    extras_require={
+        'sqlalchemy': ['SQLAlchemy']
+    }
+)
