00415e229a7b86742b4ed675b98e6b68dc9964a9
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-04-29 15:26:51 +0100

    Initial commit

---
+++ cornflake/fields.py
@@ -0,0 +1,497 @@
+import collections
+import six
+from datetime import date, datetime
+import copy
+import uuid
+
+import delorean
+
+
+empty = object()
+
+
+class ValidationError(Exception):
+    def __init__(self, errors):
+        self.errors = errors
+
+
+class SkipField(Exception):
+    pass
+
+
+class Field(object):
+    error_messages = {
+        'required': 'This field is required.',
+        'null': 'This field may not be null.'
+    }
+
+    def __init__(self, source=None, read_only=False, write_only=False, required=None, default=empty, validators=None, null=False, error_messages=None):
+        if required is None:
+            required = default is empty and not read_only
+
+        if validators is None:
+            validators = list()
+
+        assert not (read_only and write_only)
+        assert not (required and read_only)
+        assert not (required and default is not empty)
+
+        self.source = source
+        self.required = required
+        self.default = default
+        self.read_only = read_only
+        self.write_only = write_only
+        self.validators = validators
+        self.null = null
+
+        messages = dict()
+
+        for cls in reversed(self.__class__.__mro__):
+            messages.update(getattr(cls, 'error_messages', dict()))
+
+        if error_messages is not None:
+            messages.update(error_messages)
+
+        self.error_messages = messages
+
+        self.field_name = None
+        self.parent = None
+
+    def bind(self, parent, field_name=None):
+        self.field_name = field_name
+        self.parent = parent
+
+        if self.source is None:
+            self.source = field_name
+
+    def fail(self, key):
+        raise ValidationError(self.error_messages[key])
+
+    def get_attribute(self, instance):
+        try:
+            if isinstance(instance, collections.Mapping):
+                instance = instance[self.source]
+            else:
+                instance = getattr(instance, self.source)
+        except (AttributeError, KeyError) as e:
+            if not self.required and self.default is empty:
+                raise SkipField()
+
+            raise e
+
+    def get_value(self, data):
+        return data.get(self.field_name, empty)
+
+    def get_default(self):
+        default = self.default
+
+        if default is empty:
+            raise SkipField()
+
+        if callable(default):
+            default = default()
+
+        return default
+
+    def to_representation(self, value):
+        raise NotImplementedError
+
+    def to_internal_value(self, data):
+        raise NotImplementedError
+
+    def validate_empty_values(self, data):
+        if self.read_only:
+            return (True, self.get_default())
+
+        if data is empty:
+            if self.required:
+                self.fail('required')
+
+            return (True, self.get_default())
+
+        if data is None:
+            if not self.null:
+                self.fail('null')
+
+            return (True, None)
+
+        return (False, data)
+
+    def run_validation(self, data):
+        (is_empty, data) = self.validate_empty_values(data)
+
+        if is_empty:
+            return data
+
+        value = self.to_internal_value(data)
+        value = self.run_validators(value)
+
+        return value
+
+    def run_validators(self, value):
+        for validator in self.validators:
+            value = validator(value)
+
+        return value
+
+    @property
+    def root(self):
+        root = self
+
+        while root.parent is not None:
+            root = root.parent
+
+        return root
+
+
+class StringField(Field):
+    error_messages = {
+        'invalid': 'A valid string is required.'
+    }
+
+    def __init__(self, **kwargs):
+        self.trim_whitespace = kwargs.pop('trim_whitespace', True)
+        super(StringField, self).__init__(**kwargs)
+
+    def to_internal_value(self, data):
+        if isinstance(data, dict) or isinstance(data, list) or isinstance(data, bool):
+            self.fail('invalid')
+
+        value = six.text_type(data)
+
+        if self.trim_whitespace:
+            value = value.strip()
+
+        return value
+
+    def to_representation(self, value):
+        return six.text_type(value)
+
+
+class BooleanField(Field):
+    error_messages = {
+        'invalid': 'A valid boolean is required.'
+    }
+
+    TRUE_VALUES = {'t', 'true', 'y', 'yes', '1', 1, True}
+    FALSE_VALUES = {'f', 'false', 'n', 'no', '0', 0, False}
+
+    def to_internal_value(self, data):
+        if hasattr(data, 'lower'):
+            data = data.lower()
+
+        # Check for TypeError as list and dict aren't hashable
+        try:
+            if data in self.TRUE_VALUES:
+                return True
+            elif data in self.FALSE_VALUES:
+                return False
+            else:
+                self.fail('invalid')
+        except TypeError:
+            self.fail('invalid')
+
+    def to_representation(self, value):
+        return bool(value)
+
+
+class IntegerField(Field):
+    error_messages = {
+        'invalid': 'A valid integer is required.'
+    }
+
+    def to_internal_value(self, data):
+        if isinstance(data, basestring):
+            data = data.strip()
+
+            if len(data) == 0:
+                self.fail('invalid')
+
+        try:
+            value = int(data)
+            value_f = float(data)
+
+            # No floats
+            if value != value_f:
+                self.fail('invalid')
+        except (ValueError, TypeError):
+            self.fail('invalid')
+
+        return value
+
+    def to_representation(self, value):
+        return int(value)
+
+
+class FloatField(Field):
+    error_messages = {
+        'invalid': 'A valid number is required.'
+    }
+
+    def to_internal_value(self, data):
+        if isinstance(data, basestring):
+            data = data.strip()
+
+            if len(data) == 0:
+                self.fail('invalid')
+
+        try:
+            value = float(data)
+        except (ValueError, TypeError):
+            self.fail('invalid')
+
+        return value
+
+    def to_representation(self, value):
+        return float(value)
+
+
+class DateField(Field):
+    error_messages = {
+        'invalid': 'Date has wrong format.',
+        'datetime': 'Expected a date but got a datetime.',
+    }
+
+    def to_internal_value(self, data):
+        if isinstance(data, datetime):
+            self.fail('datetime')
+        elif isinstance(data, date):
+            # Already a date
+            return data
+        elif not isinstance(data, six.string_types):
+            # Not a string
+            self.fail('invalid')
+        else:
+            try:
+                value = delorean.parse(data).date
+            except ValueError:
+                self.fail('invalid')
+
+            return value
+
+    def to_representation(self, value):
+        # TODO always %Y-%m-%d
+        return value.isoformat()
+
+
+class DateTimeField(Field):
+    error_messages = {
+        'invalid': 'Datetime has wrong format.',
+        'date': 'Expected a date but got a datetime.',
+    }
+
+    def to_internal_value(self, data):
+        if isinstance(data, datetime):
+            # Already a datetime
+            return data
+        elif isinstance(data, date):
+            self.fail('date')
+        elif not isinstance(data, six.string_types):
+            # Not a string
+            self.fail('invalid')
+        else:
+            try:
+                value = delorean.parse(data).datetime
+            except ValueError:
+                self.fail('invalid')
+
+            return value
+
+    def to_representation(self, value):
+        # TODO always %Y-%m-%dT%H:%M:%S+00:00
+        return value.isoformat()
+
+
+class ListField(Field):
+    child_field = None
+
+    error_messages = {
+        'not_a_list': 'Expected a list.'
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.child_field = kwargs.pop('child_field', copy.deepcopy(self.child_field))
+        assert self.child is not None
+        super(ListField, self).__init__(*args, **kwargs)
+        self.child_field.bind(self)
+
+    def to_internal_value(self, data):
+        if not isinstance(data, list):
+            self.fail('not_a_list')
+
+        values = []
+        errors = {}
+
+        for i, x in enumerate(data):
+            try:
+                value = self.child_field.to_internal_value(x)
+            except ValidationError as e:
+                errors[i] = e.errors
+            else:
+                values.append(value)
+
+        if errors:
+            raise ValidationError(errors)
+
+        return values
+
+    def to_representation(self, values):
+        data = []
+
+        for value in values:
+            data.append(self.child_field.to_representation(value))
+
+        return data
+
+
+class CommaSeparatedField(Field):
+    child_field = None
+
+    error_messages = {
+        'invalid': 'A valid string is required.'
+    }
+
+    def __init__(self, field, **kwargs):
+        self.child = kwargs.pop('child_field', copy.deepcopy(self.child_field))
+        assert self.child_field is not None
+        super(CommaSeparatedField, self).__init__(**kwargs)
+        self.child_field.bind(self)
+
+    def to_internal_value(self, data):
+        if isinstance(data, dict) or isinstance(data, list) or isinstance(data, bool):
+            self.fail('invalid')
+
+        parts = six.text_type(data).split(',')
+        values = []
+
+        for part in parts:
+            value = self.child_field.to_value(part)
+            values.append(value)
+
+        return values
+
+    def to_representation(self, values):
+        return ','.join(six.text_type(value) for value in values)
+
+
+class UUIDField(Field):
+    error_messages = {
+        'invalid': 'A valid UUID is required.'
+    }
+
+    def to_internal_value(self, data):
+        if isinstance(data, dict) or isinstance(data, list) or isinstance(data, bool):
+            self.fail('invalid')
+
+        value = six.text_type(data)
+
+        try:
+            uuid.UUID(data)
+        except ValueError:
+            self.fail('invalid')
+
+        return value
+
+    def to_representation(self, value):
+        return six.text_type(value)
+
+
+class EnumField(Field):
+    error_messages = {
+        'invalid': 'Not a valid value.'
+    }
+
+    def __init__(self, enum, **kwargs):
+        super(EnumField, self).__init__(**kwargs)
+        self.enum = enum
+
+    def to_internal_value(self, data):
+        try:
+            value = self.enum(data)
+        except ValueError:
+            self.fail('invalid')
+
+        return value
+
+    def to_representation(self, value):
+        try:
+            data = value.value
+        except AttributeError:
+            data = value
+
+        return data
+
+
+class LookupField(Field):
+    error_messages = {
+        'invalid': 'Not a valid value.'
+    }
+
+    def __init__(self, key_field, items, key_name='key', value_name='value', **kwargs):
+        super(LookupField, self).__init__(**kwargs)
+
+        self.key_field = key_field
+        self.key_field.bind(self)
+
+        self.value_field = StringField()
+        self.value_field.bind(self)
+
+        self.items = items
+
+        self.key_name = key_name
+        self.value_name = value_name
+
+    def to_internal_value(self, data):
+        if isinstance(data, dict):
+            data = data.get(self.key_name, empty)
+
+            if data is empty:
+                self.fail('required')
+            elif data is None:
+                if None in self.items:
+                    value = data
+                else:
+                    self.fail('required')
+            else:
+                value = self.key_field.to_internal_value(data)
+        else:
+            value = self.key_field.to_internal_value(data)
+
+        if value not in self.items.keys():
+            self.fail('invalid')
+
+        return value
+
+    def to_representation(self, key):
+        value = self.items[key]
+
+        return {
+            self.key_name: self.key_field.to_representation(key),
+            self.value_name: self.value_field.to_representation(value)
+        }
+
+
+class StringLookupField(LookupField):
+    def __init__(self, items, **kwargs):
+        key_field = StringField()
+        super(StringLookupField, self).__init__(key_field, items, **kwargs)
+
+
+class IntegerLookupField(LookupField):
+    def __init__(self, items, **kwargs):
+        key_field = IntegerField()
+        super(IntegerLookupField, self).__init__(key_field, items, **kwargs)
+
+
+class EnumLookupField(LookupField):
+    def __init__(self, enum, items, **kwargs):
+        key_field = EnumField(enum)
+        super(EnumLookupField, self).__init__(key_field, items, **kwargs)
+
+
+class JSONField(Field):
+    def to_internal_value(self, data):
+        return data
+
+    def to_representation(self, value):
+        return value
---
+++ cornflake/serializers.py
@@ -0,0 +1,224 @@
+import copy
+
+from fields import Field, ValidationError, SkipField, empty
+
+
+class BaseSerializer(Field):
+    def __init__(self, instance=None, data=None, **kwargs):
+        meta = getattr(self, 'Meta', None)
+
+        if meta is not None:
+            default_validators = getattr(meta, 'validators', None)
+
+            if default_validators is not None and kwargs.get('validators') is None:
+                kwargs['validators'] = default_validators
+
+        super(BaseSerializer, self).__init__(**kwargs)
+
+        if data is None:
+            data = dict()
+
+        self.instance = instance
+        self.initial_data = data
+
+        self.errors = {}
+        self.validated_data = {}
+
+    def is_valid(self, raise_exception=False):
+        try:
+            self.validated_data = self.run_validation(self.initial_data)
+        except ValidationError as e:
+            self.validated_data = {}
+            self.errors = e.errors
+        else:
+            self.errors = {}
+
+        if self.errors and raise_exception:
+            raise ValidationError(self.errors)
+
+        return not bool(self.errors)
+
+    @property
+    def data(self):
+        if self.instance is not None:
+            data = self.to_representation(self.instance)
+        elif self.validated_data:
+            data = self.to_representation(self.validated_data)
+        else:
+            data = {}
+
+        return data
+
+    def create(self, validated_data):
+        raise NotImplementedError
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError
+
+    def save(self, **kwargs):
+        validated_data = dict(
+            list(self.validated_data.items()) +
+            list(kwargs.items())
+        )
+
+        if self.instance is None:
+            self.instance = self.create(validated_data)
+        else:
+            self.instance = self.update(self.instance, validated_data)
+
+        return self.instance
+
+
+class Serializer(BaseSerializer):
+    error_messages = {
+        'not_a_dict': 'Expected an object.'
+    }
+
+    def get_fields(self):
+        raise NotImplementedError
+
+    @property
+    def fields(self):
+        fields = self.get_fields()
+
+        for field_name, field in fields.items():
+            field.bind(self, field_name)
+
+        return fields
+
+    @property
+    def writable_fields(self):
+        return [
+            field for field in self.fields.values()
+            if not field.read_only or field.default is not empty
+        ]
+
+    @property
+    def readable_fields(self):
+        return [
+            field for field in self.fields.values()
+            if not field.write_only
+        ]
+
+    def run_validation(self, data):
+        value = self.to_internal_value(data)
+
+        try:
+            value = self.run_validators(value)
+            value = self.validate(value)
+        except ValidationError as e:
+            if isinstance(e.errors, dict):
+                raise
+            else:
+                raise ValidationError({'_': e.errors})
+
+        return value
+
+    def validate(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        if not isinstance(data, dict):
+            self.fail('not_a_list')
+
+        errors = {}
+        value = {}
+
+        for field in self.writable_fields:
+            field_value = field.get_value(data)
+            validate_method = getattr(self, 'validate_' + field.field_name, None)
+
+            try:
+                field_value = field.run_validation(field_value)
+
+                if validate_method is not None:
+                    field_value = validate_method(field_value)
+            except ValidationError as e:
+                errors[field.field_name] = e.errors
+            except SkipField:
+                pass
+            else:
+                value[field.source] = field_value
+
+        if errors:
+            raise ValidationError(errors)
+
+        return value
+
+    def to_representation(self, instance):
+        data = dict()
+
+        for field in self.readable_fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+
+            if attribute is None:
+                data[field.field_name] = None
+            else:
+                data[field.field_name] = field.to_representation(attribute)
+
+        return data
+
+
+class ListSerializer(BaseSerializer):
+    child_serializer = None
+
+    default_error_messages = {
+        'not_a_list': 'Expected a list.'
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.child_serializer = kwargs.pop('child_serializer', copy.deepcopy(self.child_serializer))
+        assert self.child_serializer is not None
+        super(ListSerializer, self).__init__(*args, **kwargs)
+        self.child_serializer.bind(self)
+
+    def run_validation(self, data):
+        value = self.to_internal_value(data)
+
+        try:
+            value = self.run_validators(value)
+            value = self.validate(value)
+        except ValidationError as e:
+            if isinstance(e.errors, dict):
+                raise
+            else:
+                raise ValidationError({'_': e.errors})
+
+        return value
+
+    def validate(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        if not isinstance(data, list):
+            self.fail('not_a_list')
+
+        values = []
+        errors = {}
+
+        for i, x in enumerate(data):
+            try:
+                value = self.child_serializer.to_internal_value(x)
+            except ValidationError as e:
+                errors[i] = e.errors
+            else:
+                values.append(value)
+
+        if errors:
+            raise ValidationError(errors)
+
+        return values
+
+    def to_representation(self, values):
+        data = []
+
+        for value in values:
+            data.append(self.child_serializer.to_representation(value))
+
+        return data
+
+    def create(self, validated_data):
+        return [self.child_serializer.create(value) for value in validated_data]
